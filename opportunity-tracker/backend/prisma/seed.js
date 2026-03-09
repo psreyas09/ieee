5bb4b2b5193704def6060ed7d9c3d67e32206f0d
psreyas09 <psreyas09@gmail.com>
2026-03-09 20:56:09 +0530

    chore: inject 10 global IEEE Regions into database seeds

--- opportunity-tracker/backend/prisma/seed.js
+++ opportunity-tracker/backend/prisma/seed.js
@@ -57,6 +57,19 @@ const councils = [
     { name: 'Future Networks', web: 'https://futurenetworks.ieee.org' }
 ];
 
+const regions = [
+    { name: 'IEEE Region 1 (Northeastern US)', web: 'https://ieeer1.org/' },
+    { name: 'IEEE Region 2 (Eastern US)', web: 'https://r2.ieee.org/' },
+    { name: 'IEEE Region 3 (Southeastern US)', web: 'https://r3.ieee.org/' },
+    { name: 'IEEE Region 4 (Central US)', web: 'https://r4.ieee.org/' },
+    { name: 'IEEE Region 5 (Southwestern US)', web: 'https://r5.ieee.org/' },
+    { name: 'IEEE Region 6 (Western US)', web: 'https://ieee-region6.org/' },
+    { name: 'IEEE Region 7 (Canada)', web: 'https://r7.ieee.org/' },
+    { name: 'IEEE Region 8 (Europe, Middle East, Africa)', web: 'https://www.ieeer8.org/' },
+    { name: 'IEEE Region 9 (Latin America)', web: 'https://www.ewh.ieee.org/reg/9/' },
+    { name: 'IEEE Region 10 (Asia and Pacific)', web: 'https://www.ieeer10.org/' }
+];
+
 async function main() {
     console.log('Seeding organizations...');
 
@@ -76,6 +89,16 @@ async function main() {
                 name: council.name,
                 type: 'council',
                 officialWebsite: council.web,
+            }
+        });
+    }
+
+    for (const region of regions) {
+        await prisma.organization.create({
+            data: {
+                name: region.name,
+                type: 'region',
+                officialWebsite: region.web,
             }
         });
     }
